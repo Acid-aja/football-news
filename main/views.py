c8fa7913270842bcb9f8dd7a492db5fb9bf3da7f
Acid-aja <izzdn19@gmail.com>
2025-09-24 15:43:45 +0700

    fix minor problem

--- main/views.py
+++ main/views.py
@@ -58,16 +58,6 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
-def create_news(request):
-    form = NewsForm(request.POST or None)
-
-    if form.is_valid() and request.method == "POST":
-        form.save()
-        return redirect('main:show_main')
-
-    context = {'form': form}
-    return render(request, "create_news.html", context)
-
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
